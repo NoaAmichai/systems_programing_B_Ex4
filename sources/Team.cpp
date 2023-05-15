e4c843f8dd30b5325c560569121e90f8462c84f7
Noa Amichai <noa1254@gmail.com>
2023-05-16 00:12:15 +0300

    .

--- sources/Team.cpp
+++ sources/Team.cpp
@@ -3,16 +3,20 @@
 
 using namespace std;
 namespace ariel {
+
+    // Constructor
     Team::Team(Character *leader) : _leader(leader), _members(0), current_members(1) {
         _members.push_back(_leader);
     }
 
+    // Destructor
     Team::~Team() {
         for (Character *member: _members) {
             delete member;
         }
     }
 
+    // Adds a member to the team
     void Team::add(Character *member) {
         if (current_members < MAX_MEMBERS) {
             ++current_members;
@@ -20,6 +24,7 @@ namespace ariel {
         } else throw runtime_error("Team is already at maximum capacity");
     }
 
+    // Attacks another team
     void Team::attack(Team *enemies) { //TODO !
         // If leader isn't alive, generate new leader
         if (!_leader->isAlive()) {
@@ -33,6 +38,14 @@ namespace ariel {
 
             // Attack the victim
             while (victim->isAlive()) {
+                if(auto* cowboy = findNextCowboyWithBullets()){
+                    cowboy->shoot(victim);
+                    break;
+                }
+                else{ //There isn't any cowboy alive and with bullets
+
+
+                }
 
             }
 //            enemies->_members.emplace_back(victim);
@@ -42,6 +55,7 @@ namespace ariel {
 
     }
 
+    // Returns the number of alive members in the team
     int Team::stillAlive() const {
         int count = 0;
         for (Character *member: _members) {
@@ -52,6 +66,7 @@ namespace ariel {
         return count;
     }
 
+    // Prints the information of all members of the team
     void Team::print() const {
         std::string str;
         for (Character *member: _members) {
@@ -60,11 +75,12 @@ namespace ariel {
         cout << str;
     }
 
+    // Finds the closest alive character to the source character
     Character *Team::findClosestCharacter(Character *source, vector<Character *> &chars) {
         Character *closet = nullptr;
         double min_distance = INT_MAX;
         for (Character *member: chars) {
-            //Check that the member is still alive and that he is not the source
+            // Check that the member is still alive and that it is not the source
             if (member == source || !member->isAlive()) continue;
 
             double current_distance = source->distance(member);
@@ -76,10 +92,17 @@ namespace ariel {
         return closet;
     }
 
+    // Finds the next alive cowboy with bullets
     Cowboy *Team::findNextCowboyWithBullets() {
-        for(Character* member :_members){
-//            if(typeid(member).name() == "Cowboy" ) {}
+        for (Character *member: _members) {
+            // Check if the member is a cowboy and has bullets
+            if (auto *cowboy = dynamic_cast<Cowboy *>(member)) { // cowboy will be null if the member isn't cowboy
+                if (cowboy->isAlive() && cowboy->hasboolets()) {
+                    return cowboy;
+                }
+            }
         }
+        return nullptr;
     }
 }
 
